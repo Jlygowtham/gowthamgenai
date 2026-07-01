788f806295b3ade1b07ffe8caff82761026c3ac4
Gowtham E <91894759+Jlygowtham@users.noreply.github.com>
2026-07-01 19:53:20 +0530

    Add OpenAI client setup and chat completion example

---
+++ 2_openrouter_code_demo/main.py
@@ -0,0 +1,23 @@
+from openai import OpenAI
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+# client = OpenAI(
+#     base_url="https://openrouter.ai/api/v1",
+#     api_key=os.getenv('OPEN_ROUTER_API'),
+# )
+
+client = OpenAI(
+    base_url="https://openrouter.ai/api/v1",
+    api_key="", #paste your api key here
+)
+
+response = client.chat.completions.create(
+    model="google/gemma-4-31b-it:free",
+    messages=[{"role": "user", "content": "Why do LLMs hallucinate?"}],
+    max_tokens = 1000
+)
+
+print(response.choices[0].message.content)
